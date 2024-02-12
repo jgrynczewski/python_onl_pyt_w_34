3cdf0d83f4903b4892e62558476dc3be6930dbf2
jgrynczewski <jgrynczewski@gmail.com>
2024-02-12 15:54:36 +0100

    Dzien_1/04_Funkcje - zadanie 1

--- 01_Dzien_1/04_Funkcje/01_Zadanie_1/exercise_1.py
+++ 01_Dzien_1/04_Funkcje/01_Zadanie_1/exercise_1.py
@@ -1,7 +1,23 @@
 # write the function from exercise 1 here
+# pure function
+def square(num):
+    result = num ** 2
+    return result
+
+
+# side-effect function
+def square_print(num):
+    result = num ** 2
+    print(result)
+
 
 # the lines below will test your function:
 print("2 squared is:", square(2))  # should be 4
 print("16^2=", square(16))  # should be 256
 print("256 to the 2nd power =", square(256))  # should be 65536
- 
+
+a = square(10) + 10
+print(a)
+
+# TypeError
+# b = square_print(10) + 10
