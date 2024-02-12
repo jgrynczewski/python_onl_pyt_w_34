1fd454d883d1f5562ddfb735ce5e07e0882af64b
jgrynczewski <jgrynczewski@gmail.com>
2024-02-12 15:55:00 +0100

    Dzien_1/04_Funkcje - zadanie 4

--- 01_Dzien_1/04_Funkcje/04_Zadanie_4/exercise_4.py
+++ 01_Dzien_1/04_Funkcje/04_Zadanie_4/exercise_4.py
@@ -1,4 +1,9 @@
 # write the function from exercise 4 here
+def convert_to_usd(euros):
+
+    result = euros / 0.82
+    return result
+
 
 # the lines below will test your function:
 print("385EUR = ", convert_to_usd(385), "USD")
