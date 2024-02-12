f7ee80ebd0d59ecfbf23286ae5a5aed6833148b5
jgrynczewski <jgrynczewski@gmail.com>
2024-02-12 15:54:55 +0100

    Dzien_1/04_Funkcje - zadanie 3

--- 01_Dzien_1/04_Funkcje/03_Zadanie_3/exercise_3.py
+++ 01_Dzien_1/04_Funkcje/03_Zadanie_3/exercise_3.py
@@ -1,6 +1,8 @@
 # write the function from exercise 3 here
+def power(base, exponent=2):
+    return base ** exponent
+
 
 # the lines below will test your function:
 print("2 to the 6th power is", power(2, 6))  # 64
 print("4 to the 8th power is", power(4, 8))  # 65536
-
