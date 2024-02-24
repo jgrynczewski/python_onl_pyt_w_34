df8c94abf0f32d651247b23e817a1b926881ed69
jgrynczewski <jgrynczewski@gmail.com>
2024-02-24 09:56:42 +0100

    Dzien_2 membership operator

---
+++ membership_operator.py
@@ -0,0 +1,12 @@
+# def check(numbers, target):
+#     for num in numbers:
+#         if num == target:
+#             return True
+#
+#     return False
+#
+#
+# print(check([1,2,3,4,5], 10))
+
+result = 10 in [1,2,3,4]
+print(result)
