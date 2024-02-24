fdc3eefd1db3063e7507a9f1eb92e226047815c6
jgrynczewski <jgrynczewski@gmail.com>
2024-02-24 15:39:38 +0100

    Dzien_2 zad 2 wyjatki

--- 05_Dzien_3/02_Obsluga_wyjatkow/02_Zadanie_2/exercise_2.py
+++ 05_Dzien_3/02_Obsluga_wyjatkow/02_Zadanie_2/exercise_2.py
@@ -5,7 +5,11 @@ rnd = randint(1, 10)
 
 while not guessed:
     str_num = input("Enter number:")
-    num = int(str_num)
+    try:
+        num = int(str_num)
+    except ValueError:
+        print("Enter valid number.")
+
     if num == rnd:
         print("Great!")
         guessed = True
