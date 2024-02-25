4bff8dc798b1c1f7870f6f036b88dab21002eef3
jgrynczewski <jgrynczewski@gmail.com>
2024-02-25 14:56:46 +0100

    Dzien_4 zad 3 (Flask)

--- 07_Dzien_4/01_Flask/exercises.py
+++ 07_Dzien_4/01_Flask/exercises.py
@@ -17,6 +17,11 @@ def time_view():
     return now.strftime("%H:%M:%S")
 
 
+@app.route("/date/")
+def date_view():
+    now = datetime.now()
+    return now.strftime("%d %b %Y")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
